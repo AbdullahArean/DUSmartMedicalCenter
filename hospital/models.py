88e19dba84160a23566d2bbfe09f38daa6eb8946
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2023-11-13 22:21:49 +0600

    models.py finalized

--- hospital/models.py
+++ hospital/models.py
@@ -49,48 +49,6 @@ class Patient(Person):
         return "{} ({})".format(self.get_name, self.symptoms)
 
 
-# class Doctor(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     profile_pic = models.ImageField(upload_to='profile_pic/DoctorProfilePic/', null=True, blank=True)
-#     address = models.CharField(max_length=40)
-#     mobile = models.CharField(max_length=20, null=True)
-#     department = models.CharField(max_length=50, choices=DepartmentStrategy.get_choices(), default='Cardiologist')
-#     status = models.BooleanField(default=False)
-#
-#     @property
-#     def get_name(self):
-#         return self.user.first_name + " " + self.user.last_name
-#
-#     @property
-#     def get_id(self):
-#         return self.user.id
-#
-#     def __str__(self):
-#         return "{} ({})".format(self.user.first_name, self.department)
-#
-#
-# class Patient(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     profile_pic = models.ImageField(upload_to='profile_pic/PatientProfilePic/', null=True, blank=True)
-#     address = models.CharField(max_length=40)
-#     mobile = models.CharField(max_length=20, null=False)
-#     symptoms = models.CharField(max_length=100, null=False)
-#     assignedDoctorId = models.PositiveIntegerField(null=True)
-#     admitDate = models.DateField(auto_now=True)
-#     status = models.BooleanField(default=False)
-#
-#     @property
-#     def get_name(self):
-#         return self.user.first_name + " " + self.user.last_name
-#
-#     @property
-#     def get_id(self):
-#         return self.user.id
-#
-#     def __str__(self):
-#         return self.user.first_name + " (" + self.symptoms + ")"
-
-
 class Appointment(DocumentBaseFactory):
     common_variable = "Appointment"
 
